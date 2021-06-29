522799b473e8a7d71e320e1ef1a9af9a2286de28
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-06-29 17:01:39 +0200

    1 & 2

--- index.js
+++ index.js
@@ -6,15 +6,53 @@ const loadFunc = async() => {
     let respData = await fetch('https://jsonplaceholder.typicode.com/users')
     let resp = await respData.json()
     console.log(resp)
-    let ul = document.querySelector('.list-group')
 
-    resp.forEach(element => {
-        //console.log(element)
-        let li = document.createElement('li')
-        li.classList.add('list-group-item')
-        li.innerText += element.name
-        ul.appendChild(li)
+    
+    
+    const displayHtml = function () {
+        let row = document.querySelector(".row")
+        resp.forEach(ele => {
+            let col = document.createElement("div")
+            col.classList.add("col-12", "col-md-3", "mb-3", "d-flex")
+            col.insertAdjacentHTML(`afterbegin`, `<div class="card" style="width: 18rem;">
+            
+            <div class="card-body">
+            <h5 class="card-title">Name: <strong>${ele.name}</strong></h5>
+            <p class="card-text"> User Name: <strong>${ele.username}</strong></p>
+            <p class="card-text"> Email: <strong>${ele.email}</strong></p>
+            <p class="card-text"> Phone: <strong>${ele.phone}</strong></p>
+            <p class="card-text">Website: <strong>${ele.website}</strong></p>
+            <p class="card-text">Company Name: <strong>${ele.company.name}</strong></p>
+            </div>
+          </div>`)
+          row.appendChild(col)
+    
+          
+        });
+    }
+    displayHtml()
+
+    let theSearch = document.querySelector(".form-control")
+    theSearch.addEventListener("keyup", function(){
+      let search = this.value.toLowerCase()
+      let allH5 = document.querySelectorAll("h5")
+      let cols = document.querySelectorAll(".col-12")
+      
+      for (let i of allH5){
+        let item = i.innerHTML.toLowerCase()
+        if (item.indexOf(search) == -1){i.parentElement.parentElement.classList.add("d-none")}
+        else {i.parentElement.parentElement.classList.remove("d-none")}
+      }
     })
+
+
+
+
+
+
+
+
+
 
     let dropDown = document.querySelector('.dropdown')
     let div = document.createElement('div')
@@ -37,16 +75,10 @@ const loadFunc = async() => {
                 //input.classList.add('d-block')
         })
     })
-
-    let inputTag = document.querySelector('input').value
-    let pTag = document.createElement('p')
-    div.appendChild(pTag)
-
-    const result = resp.filter(user => {
-        if (inputTag === user.name) {
-            pTag.innerText = user
-        }
-    })
+    
+    
+    
+    
 
 
 
